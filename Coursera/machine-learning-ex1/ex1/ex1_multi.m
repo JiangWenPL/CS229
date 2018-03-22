771c23dd53a7a91e3f5e44ae770ac8e75d062aad
JiangWenPL <JiangWenPL@users.noreply.github.com>
2018-03-22 19:35:12 +0800

    Week 1 Advanced done

--- Coursera/machine-learning-ex1/ex1/ex1_multi.m
+++ Coursera/machine-learning-ex1/ex1/ex1_multi.m
@@ -94,7 +94,13 @@ figure;
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
-
+hold on;
+% theta2 = zeros(3, 1);
+% [theta2, J_history2] = gradientDescentMulti(X, y, theta2, alpha/2, num_iters);
+% plot(1:numel(J_history2), J_history2, '-r', 'LineWidth', 2);
+% theta3 = zeros(3, 1);
+% [theta3, J_history3] = gradientDescentMulti(X, y, theta3, alpha*2, num_iters);
+% plot(1:numel(J_history2), J_history3, '-k', 'LineWidth', 2);
 % Display gradient descent's result
 fprintf('Theta computed from gradient descent: \n');
 fprintf(' %f \n', theta);
@@ -104,7 +110,7 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+price = [1, 1650, 3]*theta; % You should change this
 
 
 % ============================================================
@@ -149,8 +155,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
-
+price = [1, 1650, 3]*theta; % You should change this
 
 % ============================================================
 
