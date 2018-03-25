7e51b19b878de294e88c5a52f6cd2f034804594b
JiangWenPL <JiangWenPL@users.noreply.github.com>
2018-03-25 09:12:08 +0800

    It shall be done, but not accuracy

--- Coursera/machine-learning-ex2/ex2/costFunctionReg.m
+++ Coursera/machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,7 +17,7 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 J = (sum(-y.*log(sigmoid(X*theta))-(1-y).*log(1-sigmoid(X*theta))))./m + lambda*(theta'*theta)./(2*m) + J;
-grad = sum((sigmoid(X * theta)-y).*X,2)./m + lambda * theta./m + grad;
+grad = sum((sigmoid(X * theta)-y).*X,1)'./m + lambda * theta./m + grad;
 
 
 
